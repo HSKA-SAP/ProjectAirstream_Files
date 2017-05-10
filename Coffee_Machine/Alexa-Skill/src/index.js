68b87cb8c7e482034e6c5a00c3fa96cd9396bbe4
Benny <benjamin.teetsch@yahoo.de>
2017-05-10 22:48:00 +0200

    Corrected Utterances and CHANGE Skill wo Call our web Service

--- Coffee_Machine/Alexa-Skill/src/index.js
+++ Coffee_Machine/Alexa-Skill/src/index.js
@@ -2,6 +2,9 @@
 
 var Alexa = require('alexa-sdk');
 
+
+
+
 var handlers = {
   
   // LaunchRequest is called if the customer opens the skill without parameters. For example: "Alexa, start coffeemachine"
@@ -55,9 +58,21 @@ var handlers = {
 	// MixedOrderIntent is called if the customer says something what we have defined in the intent-section of Amazon Developer Account and tagged with "MixedOrderIntent"
   'MixedOrderIntent': function() {
 	  
-	  //To-DO Call function with parameters for one coffee and one hot water
-	  var speechOut = 'Gerne, ich werde dir einen Kaffee und eine Tasse heißes Wasser machen';
-	  this.emit(':tell', speechOut);
+	  var quantityCoffee = 1;
+	  var quantityWater = 1;
+	  
+	  httpGet(quantityCoffee, quantityWater, (statusCode) => {
+                
+				if (statusCode == 200){
+					this.emit(':tell', 'Gerne, ich werde Ihnen einen Kaffee und eine Tasse heißes Wasser machen' );
+				}
+				else
+					this.emit(':tell', 'Tut mir Leid, die Bestellung hat nicht funktioniert');
+
+            }
+        );
+	
+	  
 	  
   },
   
@@ -67,16 +82,85 @@ var handlers = {
   },
   
   'ConfirmationIntent': function(quantity, drink) {
+		var quantityWater = 0;
+		var quantityCoffee = 0;
+		
+		if (drink == "heißes Wasser") {
+			quantityWater = quantity;
+		}
+		else
+			quantityCoffee = quantity;
 	  
-	  
-	  //To-Do Call the function for making coffee with parameters "quantity" and "drink"
-	  var speechOut = 'Alles klar, ich werde dir ' + (quantity == 2 ? "zwei Tassen" : "eine Tasse") + ' ' + drink + ' zubereiten';
-      this.emit(':tell', speechOut);
+		httpGet(quantityCoffee, quantityWater, (statusCode) => {
+                
+				if (statusCode == 200){
+					this.emit(':tell', 'Alles klar, ich werde Ihnen ' + (quantity == 2 ? "zwei Tassen" : "eine Tasse") + ' ' + drink + ' zubereiten' );
+				}
+				else
+					this.emit(':tell', 'Tut mir Leid, die Bestellung hat nicht funktioniert');
+
+            }
+        );
   }  
+
 };
+
+
 
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
+
+
+var http = require('http');
+
+
+function httpGet(quantityCoffee, quantityWater, callback) {
+
+    // GET is a web service request that is fully defined by a URL string
+    // Try GET in your browser:
+    // https://cp6gckjt97.execute-api.us-east-1.amazonaws.com/prod/stateresource?usstate=New%20Jersey
+
+
+    // Update these options with the details of the web service you would like to call
+    var options = {
+        host: 'airstream.hopto.org',
+        port: 80,
+        path: '/cgi-bin/coffeeCGI.py?coffee=' + quantityCoffee + '&water=' + quantityWater,
+        method: 'GET',
+
+        // if x509 certs are required:
+        // key: fs.readFileSync('certs/my-key.pem'),
+        // cert: fs.readFileSync('certs/my-cert.pem')
+    };
+
+    var req = http.request(options, res => {
+        res.setEncoding('utf8');
+        
+		
+		var returnData = "";
+
+		
+        res.on('data', chunk => {
+            returnData = returnData + chunk;
+        });
+
+        res.on('end', () => {
+            // we have now received the raw return data in the returnData variable.
+            // We can see it in the log output via:
+            // console.log(JSON.stringify(returnData))
+            // we may need to parse through it to extract the needed data
+
+            
+
+            callback(res.statusCode);  // this will execute whatever function the caller defined, with one argument
+
+        });
+
+    });
+    req.end();
+
+}
+
