953c4e5f8ad7dca49bfce436663fb9ad9bba72e8
chrisweisgerber <christopher-weisgerber@web.de>
2017-05-19 21:11:19 +0200

    Update QuestionsAndAnswers.cs
    Added class Answer

--- Feedback-Screen/Airstream/Feedback/Q and A/QuestionsAndAnswers.cs
+++ Feedback-Screen/Airstream/Feedback/Q and A/QuestionsAndAnswers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,13 +8,15 @@ namespace Airstream.Feedback.QuestionsAndAnswers
     {
         private static List<Question> listAllQuestion = new List<Question>();
 
+        private int _number;
         private string _question;
-        private List<string> _options;
+        private List<Answer> _options;
 
+        public int number { get { return _number; } }
         public string question { get { return _question; } }
-        public List<string> options { get { return _options; } }
+        public List<Answer> options { get { return _options; } }
 
-        public Question(string question, List<string> options)
+        public Question(string question, List<Answer> options)
         {
             if (String.IsNullOrEmpty(question))
                 throw new Exception("No Question!");
@@ -22,6 +24,7 @@ namespace Airstream.Feedback.QuestionsAndAnswers
             if (options.Count() < 2)
                 throw new Exception("Not enough options to answer the question!");
 
+            _number = listAllQuestion.Count();
             _question = question;
             _options = options;
 
@@ -36,6 +39,24 @@ namespace Airstream.Feedback.QuestionsAndAnswers
         public static void CreateTheFeedbackQuestions()
         {
 
+        }
+    }
+
+    class Answer
+    {
+        private string _text;
+        private int _countVotes;
+
+        public string text { get { return _text; } }
+        public int countVotes { get { return _countVotes; } }
+
+        public Answer(string text, int countVotes)
+        {
+            if (String.IsNullOrEmpty(text))
+                throw new Exception("No text existing!");
+
+            _text = text;
+            _countVotes = countVotes;
         }
     }
 }
