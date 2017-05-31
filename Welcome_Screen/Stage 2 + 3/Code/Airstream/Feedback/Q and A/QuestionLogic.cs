40866cb850b71013e8da4aa2f4a7648f2419d054
bojandjukic1 <bojan.djukic5@gmail.com>
2017-05-31 20:36:29 +0200

    Latest version - Added feedback controls + database connection

---
+++ Welcome_Screen/Stage 2 + 3/Code/Airstream/Feedback/Q and A/QuestionLogic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airstream.Feedback.Q_and_A_Logic
+{
+    class QuestionLogic
+    {
+
+        public static List<QuestionsAndAnswers.Question> CreateTheFeedbackQuestions()
+        {
+            QuestionsAndAnswers.Question q1 = new QuestionsAndAnswers.Question("Welches der folgenden Geräte im Airstream fanden Sie am besten?\n", new List<QuestionsAndAnswers.Answer>());
+            q1.options.Add(new QuestionsAndAnswers.Answer("Alexa"));
+            q1.options.Add(new QuestionsAndAnswers.Answer("NetAtmo"));
+            q1.options.Add(new QuestionsAndAnswers.Answer("55 Zoll-Tisch"));
+            q1.options.Add(new QuestionsAndAnswers.Answer("Welcome-Screen"));
+            q1.options.Add(new QuestionsAndAnswers.Answer("Digital Boardroom"));
+
+            QuestionsAndAnswers.Question q2 = new QuestionsAndAnswers.Question("Wie würden Sie den Gesamteindruck bewerten?\n", new List<QuestionsAndAnswers.Answer>());
+            q2.options.Add(new QuestionsAndAnswers.Answer("Sehr gut"));
+            q2.options.Add(new QuestionsAndAnswers.Answer("Gut"));
+            q2.options.Add(new QuestionsAndAnswers.Answer("Nichts besonderes"));
+            q2.options.Add(new QuestionsAndAnswers.Answer("Schlecht"));
+            q2.options.Add(new QuestionsAndAnswers.Answer("Sehr schlecht"));
+
+            QuestionsAndAnswers.Question q3= new QuestionsAndAnswers.Question("Random Question?\n", new List<QuestionsAndAnswers.Answer>());
+            q3.options.Add(new QuestionsAndAnswers.Answer("Sehr gut"));
+            q3.options.Add(new QuestionsAndAnswers.Answer("Gut"));
+            q3.options.Add(new QuestionsAndAnswers.Answer("Nichts besonderes"));
+            q3.options.Add(new QuestionsAndAnswers.Answer("Schlecht"));
+            q3.options.Add(new QuestionsAndAnswers.Answer("Sehr schlecht"));
+
+            List<QuestionsAndAnswers.Question> questions = new List<QuestionsAndAnswers.Question>();
+
+            questions.Add(q1);
+            questions.Add(q2);
+            questions.Add(q3);
+            return questions;
+        }
+
+
+
+    }
+
+
+}
---
+++ Welcome_Screen/Stage 2 + 3/Code/Airstream/Feedback/Voters/Voters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Airstream.Feedback.QuestionsAndAnswers;
+
+namespace Airstream.Feedback.Voters
+{
+    class Voter
+    {
+        private static List<Voter> listAllVoters = new List<Voter>();
+
+        private long _id;
+        private List<Answer> _givenAnswers;
+        private DateTime _dateTimeVote;
+
+        public long id { get { return _id; } }
+        public List<Answer> givenAnswers { get { return _givenAnswers; } }
+        public DateTime dateTimeVote { get { return _dateTimeVote; } }
+
+        public Voter(long id)
+        {
+            _id = id;
+            _givenAnswers = new List<Answer>();
+
+            listAllVoters.Add(this);
+        }
+
+        public static List<Voter> GetAllVoters()
+        {
+            return listAllVoters;
+        }
+
+        public void SetDateTimeVote()
+        {
+            _dateTimeVote = DateTime.Now;
+        }
+
+        public void AddGivenAnswer(Voter voter, Answer answer)
+        {
+            SetDateTimeVote();
+            voter._givenAnswers.Add(answer);
+            answer.AddCountVote();
+        }
+    }
+}
