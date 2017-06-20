561899fb5f45b62ea508e47d62fc94315ca254a1
chrisweisgerber <christopher-weisgerber@web.de>
2017-06-20 23:54:34 +0200

    Add files via upload

---
+++ Feedback-Screen/Airstream_version_of_Chris/airstream/Feedback/Database/DatabaseConnection.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Airstream.Feedback.Database
+{
+    class DatabaseConnection
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        //Constructor
+        public DatabaseConnection()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "2001:7c0:409:802:d3af:fc08:3e73:e574";
+            database = "rfid_db";
+            uid = "chris";
+            password = "MrMiller7";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+
+        //Delete statement
+        public void InsertUpdateDelete(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Select statement
+        public List<string>[] Select(string query, List<string> columnNames, List<string>[] list)
+        {
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    for(int i=0; i < list.Length; i++)
+                    {
+                        list[i].Add(dataReader[columnNames[i]] + "");
+
+                    }
+                }
+                dataReader.Close();
+                this.CloseConnection();
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        public List<string>[] SelectFromUsers(string query)
+        {
+            //Create a list to store the result
+            List<string>[] list = new List<string>[4];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+            list[3] = new List<string>();
+
+            List<string> columnNames = new List<string>();
+            columnNames.Add("userID");
+            columnNames.Add("fName");
+            columnNames.Add("lName");
+            columnNames.Add("company");
+            return Select(query, columnNames, list);
+
+        }
+
+
+
+        public List<string>[] SelectFromFeedback(string query)
+        {
+            //Create a list to store the result
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+            List<string> columnNames = new List<string>();
+            columnNames.Add("feedbackID");
+            columnNames.Add("feedbackQ");
+            columnNames.Add("feedbackAnswer");
+            return Select(query, columnNames, list);
+
+        }
+
+
+        //Count statement
+        public int Count(string query)
+        {
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                    Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+    }
+}
---
+++ Feedback-Screen/Airstream_version_of_Chris/airstream/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Airstream.Feedback.QuestionsAndAnswers;
+using Airstream.Feedback.Q_and_A_Logic;
+using Airstream.Feedback.Voters;
+using Airstream.User_Interfaces;
+using Airstream.Feedback.Database;
+
+namespace Airstream
+{
+    static class Program
+    {
+        /// <summary>
+        /// Der Haupteinstiegspunkt für die Anwendung.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Voter TestVoter = new Voter(12345);
+            List<Question> questions = QuestionLogic.CreateTheFeedbackQuestions();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UI_Feedback(questions));
+        }
+    }
+}
